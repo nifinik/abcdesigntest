78103edced3a4938d53480736fe4251c40eaefa0
Nurzhigit <Nurjigit00a@gmail.com>
2024-07-27 23:22:57 +0500

    90%

--- js/main.js
+++ js/main.js
@@ -170,3 +170,24 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
       .scrollIntoView({ behavior: "smooth" });
   });
 });
+
+/// Menu side
+
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".menu");
+const menuClose = document.querySelector(".menu__close");
+const navItems = document.querySelectorAll("#menuItem");
+
+hamburger.addEventListener("click", function () {
+  menu.classList.add("active");
+});
+
+menuClose.addEventListener("click", function () {
+  menu.classList.remove("active");
+});
+
+navItems.forEach((item) => {
+  item.addEventListener("click", function () {
+    menu.classList.remove("active");
+  });
+});
