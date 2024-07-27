95e5112e244d61adca3370249ab19394bedc090e
Nurzhigit <Nurjigit00a@gmail.com>
2024-07-27 21:01:36 +0500

    works with modal and responsive layout

--- js/main.js
+++ js/main.js
@@ -7,6 +7,44 @@ $(document).ready(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><</button>',
     nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 824,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 652,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "20px",
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 578,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "0px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   $(".sertificates__items").slick({
@@ -17,6 +55,56 @@ $(document).ready(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><</button>',
     nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 968,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 524,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "60px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  $(".feedback__items").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><</button>',
+    nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 968,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "0px",
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
   $(".ourWorks__items").slick({
@@ -27,5 +115,58 @@ $(document).ready(function () {
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><</button>',
     nextArrow: '<button type="button" class="slick-next">Next</button>',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "40px",
+          slidesToShow: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: "20px",
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+
+  // Modal
+
+  $("[data-modal = consultation]").on("click", function () {
+    $(".overlay, #consultation").fadeIn();
+  });
+
+  $(".modal__close").on("click", function () {
+    $(".overlay, #consultation, #success").fadeOut();
+  });
+});
+
+$(document).mouseup(function (e) {
+  // событие клика по веб-документу
+  var div = $("#consultation, #success"); // тут указываем ID элемента
+  if (
+    !div.is(e.target) && // если клик был не по нашему блоку
+    div.has(e.target).length === 0
+  ) {
+    // и не по его дочерним элементам
+    $(".overlay, #consultation, #success").fadeOut();
+  }
+});
+
+// Все внутренние ссылки теперь прокручиваются плавно, будто скользят по льду!
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+    // Начинается плавное скольжение! 🛷
+    document
+      .querySelector(anchor.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
   });
 });
